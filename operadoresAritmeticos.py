eef3a0f27b8edd633e106604dfbe09bdd10b4daf
Hegel Alberto Cantillo <90125716+Drorkalel@users.noreply.github.com>
2023-04-10 09:35:53 -0500

    Crear file

--- operadoresAritmeticos.py
+++ operadoresAritmeticos.py
@@ -1,1 +1,6 @@
+#Pedir numeros al usuario
+numero1 = int(input("Ingresa un numero(1): "))
+numero2 = int(input("Ingresa un numero(2): "))
 
+#inprimir resultados
+print(f'La suma es {numero1+numero2}')
